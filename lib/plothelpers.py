5cf09a016c5ecfd4669368c50ae4766b8de91742
Kasper Jordaens <kasper.jordaens@imec.be>
2019-11-25 16:22:53 +0100

    plotting dogit

--- lib/plothelpers.py
+++ lib/plothelpers.py
@@ -13,28 +13,29 @@ def sign(filename,x,y):
 
 
 def plotgroup(plotter, g,paddingfactor,zone,noisexy,pen):
-	plotter.select_pen(pen)
-	x1,y1 = zone[0]
-	x2,y2 = zone[1]
-	maxx = abs(x2-x1)
-	maxy = abs(y2-y1)
-	xfactor = maxx / g.width/paddingfactor
-	yfactor = maxy / g.height/paddingfactor
-	if (yfactor <= xfactor):
-		scale = yfactor
-		transforms.scale(g, scale)
-		# transforms.offset(g,((recordsize/plotunit - g.width)/2 , (recordsize/plotunit - g.height)/2))
-
-	else:
-		scale = xfactor
-		transforms.scale(g, scale)
-                # transforms.offset(g,((recordsize/plotunit - g.width)/2 , (recordsize/plotunit - g.height)/2))
-
-		# transforms.offset(g,(0,(y2-y1+g.height)/2))
-
-	print ("SCALE = " + str(scale))
-	transforms.offset(g, (x1,y1))
-	if not noisexy == (0,0):
-		transforms.noise(g,noisexy)
-	# plotter.write(g)
-	return g
+    bb = get_bounding_rectangle(g)
+    bb = get_minmax_coordinates(bb.points)
+    print bb
+    # print (g + " is " + str(g.width*plotunit) + "mm")
+    # print (g + " is " + str(g.height*plotunit) + "mm")
+    # plotter.write(g)
+    transforms.offset(g, (-bb[0][0], -bb[0][1] ))
+    plotter.select_pen(pen)
+    x1,y1 = zone[0]
+    x2,y2 = zone[1]
+    maxx = abs(x2-x1)
+    maxy = abs(y2-y1)
+    xfactor = maxx / g.width/paddingfactor
+    yfactor = maxy / g.height/paddingfactor
+    if (yfactor <= xfactor):
+        scale = yfactor
+        transforms.scale(g, scale)
+    else:
+        scale = xfactor
+        transforms.scale(g, scale)
+    print ("SCALE = " + str(scale))
+    transforms.offset(g, (x1,y1))
+    if not noisexy == (0,0):
+        transforms.noise(g,noisexy)
+    # plotter.write(g)
+    return g
