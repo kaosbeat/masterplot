6c63166b0dd39b2a5bc228d6640926d7c0000fae
Kasper Jordaens <kasper.jordaens@imec.be>
2020-01-13 17:23:19 +0100

    plotting dogit

--- blenderplotter/generateandrender.py
+++ blenderplotter/generateandrender.py
@@ -1,11 +1,15 @@
 import bpy
-#from chiplotle import *
-#from svgpathtools import svg2paths, Path, Line, Arc, CubicBezier, QuadraticBezier
+C = bpy.context 
+D = bpy.data
+
 import random
 import sys
 import argparse
 #import tracery
 #from tracery.modifiers import base_english
+from math import pi
+
+
 
 layers = [False]*20
 layers[0] = True
@@ -42,6 +46,73 @@ def multicubegeom(cubenum, union):
         bpy.ops.btool.auto_union(solver='BMESH')
     bpy.ops.object.select_all(action='SELECT')
     bpy.ops.view3d.camera_to_view_selected()
+
+
+def star(num_arms, arms_length):
+    x = 0.0
+    y = 0.0
+    z = 0.0
+    yrot = 0
+    cursor = (x,y,z)
+    master_collection = C.scene.collection
+    C.scene.cursor.location = cursor
+    #add_cube = bpy.ops.mesh.primitive_cube_add
+    p_up = 0.0
+    star = D.collections.new('star')
+    C.scene.collection.children.link(star)
+    rotmod = num_arms/6
+    
+    #bpy.ops.outliner.collection_new(nested=True)
+    
+    for i in range(0,num_arms,1):
+        cubesize=2
+        x = 0.0
+        y = 0.0
+        z = 0.0
+        arm = bpy.data.objects.new( 'arm' + str(i), None )
+        #layer_collection = D.collections.new('arm' + str(j))
+        #C.scene.collection.children.link(layer_collection)
+        #C.scene.collection.objects.link(arm)
+        star.objects.link(arm)
+        p_up = 1/num_arms * i
+        p_up = random.random()
+        for j in range(0,arms_length,1):
+            #layer_collection = bpy.data.collections['arm' + str(j)]
+            rot = [0,0, 2*pi/arms_length*j]
+            x = x + random.random()*cubesize-cubesize/2
+            y = y + cubesize
+            randz = random.random()
+            if (randz > p_up):
+                z = z + random.random()*cubesize
+            else:
+                z = z - random.random()*cubesize
+            loc = (x, y, z)
+            bpy.ops.mesh.primitive_cube_add(size=cubesize, enter_editmode=False, location=loc, rotation=rot)
+            star.objects.link(C.object) #link it with collection
+            armstep = C.object
+            armstep.parent = arm
+            master_collection.objects.unlink(C.object)
+            cubesize=cubesize*0.8
+        for ob in C.selected_objects:
+            ob.select_set(False)
+        print(int(i%rotmod))
+        D.objects['arm' + str(i)].select_set(True)
+        #C.scene.objects.active = D.objects['arm' + str(i)]
+        if (int(i%rotmod) == int(rotmod-1)):
+            print("rotmod =" + str(rotmod) + ", i =" + str(i))
+            yrot = yrot+1
+            print("yrot = " + str(yrot))
+        bpy.ops.transform.rotate(value = 2*pi/6*yrot, orient_axis='X' )
+        bpy.ops.transform.rotate(value = 2*pi/rotmod*i, orient_axis='Z' )
+
+    bpy.ops.object.select_all(action='SELECT')
+    # use booltool addon
+    bpy.ops.object.booltool_auto_union()
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.view3d.camera_to_view_selected()
+
+
+
 
 
 def bezierStack():
@@ -93,8 +164,8 @@ def setFreestyleContext():
 
     bpy.context.scene.render.use_freestyle = True
     #change to script mode
-    #rl = bpy.context.scene.render.layers.active
-    #rl.freestyle_settings.mode = 'EDITOR'
+    # rl = bpy.context.scene.render.layers.active
+    # rl.freestyle_settings.mode = 'EDITOR'
 
 def setRenderSize():
     bpy.context.scene.render.resolution_y = 2970 
@@ -111,7 +182,9 @@ def fitCam():
     
 def renderStuff():
     #render image
+    #C.scene.svg_export.use_svg_export = True
     bpy.ops.render.render( write_still=True )
+    
 
 
 rules = {
@@ -140,26 +213,33 @@ rules = {
 #setFreestyleContext()
 #setRenderParams()
 #renderStuff()
-#print("doing: " + sys.argv[sysargvoffset+3])
+# print("doing: " + sys.argv[sysargvoffset+3])
 
 
-#for idx,a in enumerate(sys.argv):
-#    if a == '-f':
-#        print('setting filepath')
-#        bpy.context.scene.render.filepath = './' + sys.argv[idx+1]
+for idx,a in enumerate(sys.argv):
+   if a == '-f':
+       print('setting filepath')
+       bpy.context.scene.render.filepath = '../output/' + sys.argv[idx+1]
 
-#    if a == '-g' or a == '--geom':
-#        print("hell yeah")
-#        print(sys.argv[idx+1])
-#        eval(sys.argv[idx+1])
+   if a == '-g' or a == '--geom':
+       print("hell yeah")
+       print(sys.argv[idx+1])
+       eval(sys.argv[idx+1])
 
-#    if a == '-r' or a == '--render':
-#
-#        print("rendering")
-#        setFreestyleContext()
-#        setRenderSize()
-#        renderStuff()
-dosomegeom()
-setFreestyleContext()
-setRenderSize()
-renderStuff()
+   if a == '-r' or a == '--render':
+
+       print("rendering")
+       fitCam()
+       setFreestyleContext()
+       setRenderSize()
+       renderStuff()
+# #dosomegeom()
+
+        
+# star(12,3)
+
+# fitCam()
+
+# setFreestyleContext()
+# setRenderSize()
+# renderStuff()
