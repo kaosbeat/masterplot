d197f3aad55d32e29ac9a884ac5c2981c0fdfe23
Kasper Jordaens <kasper.jordaens@imec.be>
2021-03-09 13:58:25 +0100

    plotting with seed 21a28b4a-80d7-11eb-a0c8-acde48001122

--- projects/mintlines/mintlines.py
+++ projects/mintlines/mintlines.py
@@ -68,7 +68,7 @@ def drawMintLines(size, seed, interx, intery, xnoise, ynoise):
     for x in xrange(0,size):
         points = []
         g = shapes.group([])
-        normx = (x-(size/2))/float(size)
+        normx = (x-(size/2))/float(size/2)
         print(normx)
         # if (x == 5):
         #     print(x, size, normx)
