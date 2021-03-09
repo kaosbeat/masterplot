ab499a8bb509540fc310eea20fbc6ea30873da81
Kasper Jordaens <kasper.jordaens@imec.be>
2021-03-09 13:52:32 +0100

    plotting with seed 4f99e7e8-80d6-11eb-b896-acde48001122

--- projects/mintlines/mintlines.py
+++ projects/mintlines/mintlines.py
@@ -81,7 +81,7 @@ def drawMintLines(size, seed, interx, intery, xnoise, ynoise):
             # else: 
             #     xn = xnoise
             #     yn = ynoise
-            yoff = math.cos(math.asin(normx))*size/2
+            yoff = math.cos(math.asin(normx))*size
             ybuf = (size/2 - yoff)
             if ((y < ybuf ) or y > size-ybuf) :
                 xn = xnoise
